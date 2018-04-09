6b04e1108d8ff1fa78eed1510c53ec04f29b5d75
nick <nikki2lol@mail.ru>
2018-04-09 21:31:50 +0300

    правки по 2 заданию

--- js/stat.js
+++ js/stat.js
@@ -28,11 +28,13 @@ var renderCloud = function (ctx, x, y, color) {
 };
 
 var renderBars = function (ctx, x, y, name, resultHeight) {
-  if (name === 'Вы') {
-    ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-  } else {
-    ctx.fillStyle = 'rgba(0, 0, 255, 1)';
-  }
+  // if (name === 'Вы') {
+  //   ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+  // } else {
+  //   ctx.fillStyle = 'rgba(0, 0, 255, 1)';
+  // }
+
+  name === 'Вы' ? ctx.fillStyle = 'rgba(255, 0, 0, 1)' : ctx.fillStyle = 'rgba(0, 0, 255, 1)';
   ctx.fillRect(x, y, BAR_WIDTH, resultHeight);
 };
 
@@ -42,15 +44,11 @@ var renderNames = function (ctx, x, y, name) {
 };
 
 var renderTimes = function (ctx, x, y, time) {
-  var timeBeatiful = Math.round(time);
   ctx.fillStyle = '#000000';
-  ctx.fillText(timeBeatiful, x, y);
+  ctx.fillText(Math.round(time), x, y);
 };
 
-window.renderStatistics = function (ctx, names, times) {
-  renderCloud(ctx, CLOUD_X + 10, CLOUD_Y + 10, 'rgba(0, 0, 0, 0.7)');
-  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#ffffff');
-
+var renderContent = function (ctx, names, times) {
   ctx.font = '16px PT Mono';
   ctx.fillStyle = '#000000';
   ctx.fillText('Ура вы победили!', CLOUD_X + GAP, 40);
@@ -59,25 +57,19 @@ window.renderStatistics = function (ctx, names, times) {
   var maxTime = getMaxElement(times);
 
   for (var i = 0; i < names.length; i++) {
-    renderBars(ctx, CLOUD_X + GAP + barFullWidth * i, CLOUD_Y + GAP + (PREVIEW_HEIGHT + GAP) + SCORE_HEIGHT + BAR_HEIGHT - times[i] * BAR_HEIGHT / maxTime + GAP, names[i], times[i] * BAR_HEIGHT / maxTime);
-    renderNames(ctx, CLOUD_X + GAP + barFullWidth * i, CLOUD_Y + GAP + (PREVIEW_HEIGHT + GAP) + SCORE_HEIGHT + BAR_HEIGHT - times[i] * BAR_HEIGHT / maxTime, names[i]);
-    renderTimes(ctx, CLOUD_X + GAP + barFullWidth * i, CLOUD_HEIGHT - CLOUD_Y, times[i]);
+
+    var positionX = CLOUD_X + GAP + barFullWidth * i;
+    var positionY = CLOUD_Y + GAP + (PREVIEW_HEIGHT + GAP) + SCORE_HEIGHT + BAR_HEIGHT - times[i] * BAR_HEIGHT / maxTime
+
+    renderBars(ctx, positionX, positionY + GAP, names[i], times[i] * BAR_HEIGHT / maxTime);
+    renderNames(ctx, positionX, positionY, names[i]);
+    renderTimes(ctx, positionX, CLOUD_HEIGHT - CLOUD_Y, times[i]);
   }
-  // не стал добавлять в тело цикла на нахождение максимального элемента, т.к. в демке было сказано, что мы будем его использовать в будущем, поэтому было бы неплохо не захламлять его сторонними значениями
-  // for (var k = 0; k < times.length; k++) {
-  //   times[k] = Math.round(times[k]);
-  // }
-  //
-  // for (var i = 0; i < names.length; i++) {
-  //   if (names[i] === 'Вы') {
-  //     ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-  //   } else {
-  //     ctx.fillStyle = 'rgba(0, 0, 255, 1)';
-  //   }
-  //   ctx.fillRect(CLOUD_X + GAP + barFullWidth * i, CLOUD_Y + GAP + (PREVIEW_HEIGHT + GAP) + SCORE_HEIGHT + BAR_HEIGHT - times[i] * prop, BAR_WIDTH, times[i] * prop);
-  //
-  //   ctx.fillStyle = '#000000';
-  //   ctx.fillText(names[i], CLOUD_X + GAP + barFullWidth * i, CLOUD_HEIGHT - GAP);
-  //   ctx.fillText(times[i], CLOUD_X + GAP + barFullWidth * i, CLOUD_Y + GAP + (PREVIEW_HEIGHT + GAP) + BAR_HEIGHT - times[i] * prop);
-  // }
+};
+
+window.renderStatistics = function (ctx, names, times) {
+  renderCloud(ctx, CLOUD_X + 10, CLOUD_Y + 10, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#ffffff');
+
+  renderContent(ctx, names, times);
 };
