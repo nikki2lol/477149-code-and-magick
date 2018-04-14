1afb61f74380e5e8ff83aadd4dec8410f0546b0d
nick <nikki2lol@mail.ru>
2018-04-14 20:52:05 +0300

    Подделал задание

--- js/setup.js
+++ js/setup.js
@@ -1,60 +1,43 @@
 'use strict';
 
 // ДАНО
-var firstNames = 'Иван, Хуан Себастьян, Мария, Кристоф, Виктор, Юлия, Люпита, Вашингтон';
-var secondNames = 'да Марья, Верон, Мирабелла, Вальц, Онопко, Топольницкая, Нионго, Ирвинг';
-// var coatColor = 'rgb(101, 137, 164), rgb(241, 43, 107), rgb(146, 100, 161), rgb(56, 159, 117), rgb(215, 210, 55), rgb(0, 0, 0)';
-var eyesColor = 'black, red, blue, yellow, green';
+var FIRST_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var SECOND_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
+var COUNT = 4;
 
-// Показываю блок и копирую шаблон
+//  Показываю блок и копирую шаблон
 document.querySelector('.setup').classList.remove('hidden');
 document.querySelector('.setup-similar').classList.remove('hidden');
 var similarListElement = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
 
-// Функция для создания массива из строки
-var generateArray = function (customStroke) {
-  var customArray = customStroke.split(', ');
 
-  // if (customStroke = coatColor) {
-  //   var customArray = customStroke.split('), ');
-  // } else {
-  //   var customArray = customStroke.split(', ');
-  // }
-
-  return customArray;
+//  Функция для выбора рандомного элемента массива
+var randomize = function (arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
 };
-
-//  Само превращение строк в массивы
-var firstNameArray = generateArray(firstNames);
-var secondNameArray = generateArray(secondNames);
-// var coatColorArray = generateArray(coatColor);
-var eyesColorArray = generateArray(eyesColor);
-
-
-//  Создаю массив с рандомными именами (допустим мне их надо 4)
-var wizardNames = [];
-var generateRandomNames = function () {
-  for (var i = 0; i < 4; i++) {
-    var fullName = firstNameArray[Math.floor(Math.random() * firstNameArray.length)] + ' ' + secondNameArray[Math.floor(Math.random() * secondNameArray.length)];
-    wizardNames.push(fullName);
-  }
-};
-generateRandomNames();
 
 //  Создам объект с волшебниками
 var wizards = [];
-
-for (var i = 0; i < 4; i++) {
-  wizards.push(wizards[i].name = wizardNames[i]);
-  wizards.push(eyesColor[i].eyes = eyesColorArray(Math.floor(Math.random() * eyesColorArray.length)));
+for (var i = 0; i < COUNT; i++) {
+  var generateWizard = function () {
+    return {
+      name: randomize(FIRST_NAMES) + ' ' + randomize(SECOND_NAMES),
+      coatColor: randomize(COAT_COLOR),
+      eyesColor: randomize(EYES_COLOR)
+    };
+  };
+  wizards.push(generateWizard());
 }
 
 var renderWizard = function (wizard) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
 
   wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
-  wizardElement.querySelector('.wizard-eyes').fill = wizard.eyesColor;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
 
   return wizardElement;
 };
