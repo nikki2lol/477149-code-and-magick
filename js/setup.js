7666c538523ab0e9082da6d3a0407d74af15fae0
nick <nikki2lol@mail.ru>
2018-04-14 21:35:53 +0300

    Причесал код

--- js/setup.js
+++ js/setup.js
@@ -3,35 +3,27 @@
 // ДАНО
 var FIRST_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var SECOND_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-var EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
-var COUNT = 4;
-
-//  Показываю блок и копирую шаблон
-document.querySelector('.setup').classList.remove('hidden');
-document.querySelector('.setup-similar').classList.remove('hidden');
-var similarListElement = document.querySelector('.setup-similar-list');
-var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
-
+var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var WIZARDS_AMOUNT = 4;
+var wizards = [];
+var fragment = document.createDocumentFragment();
 
 //  Функция для выбора рандомного элемента массива
-var randomize = function (arr) {
+var getRandomArrayElement = function (arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 };
 
-//  Создам объект с волшебниками
-var wizards = [];
-for (var i = 0; i < COUNT; i++) {
-  var generateWizard = function () {
-    return {
-      name: randomize(FIRST_NAMES) + ' ' + randomize(SECOND_NAMES),
-      coatColor: randomize(COAT_COLOR),
-      eyesColor: randomize(EYES_COLOR)
-    };
+// Генерация одного волшебника
+var generateWizard = function () {
+  return {
+    name: getRandomArrayElement(FIRST_NAMES) + ' ' + getRandomArrayElement(SECOND_NAMES),
+    coatColor: getRandomArrayElement(COAT_COLORS),
+    eyesColor: getRandomArrayElement(EYES_COLORS)
   };
-  wizards.push(generateWizard());
-}
+};
 
+// Функция отрисовки волшебников в элементе
 var renderWizard = function (wizard) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
 
@@ -42,7 +34,17 @@ var renderWizard = function (wizard) {
   return wizardElement;
 };
 
-var fragment = document.createDocumentFragment();
+// Цикл для заполнения объекта волшебников
+for (var i = 0; i < WIZARDS_AMOUNT; i++) {
+  wizards.push(generateWizard());
+}
+
+//  Показываю блок и копирую шаблон
+document.querySelector('.setup').classList.remove('hidden');
+document.querySelector('.setup-similar').classList.remove('hidden');
+var similarListElement = document.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
+
 for (var k = 0; k < wizards.length; k++) {
   fragment.appendChild(renderWizard(wizards[k]));
 }
